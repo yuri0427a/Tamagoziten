f173563aa2893d3c30ef55d333280f4b9f5d262e
yuri0427a <yuri0427ita@gmail.com>
2020-03-22 09:14:13 +0000

    [Add]ルーディング

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,43 @@
 Rails.application.routes.draw do
-  get 'cooking_materials/index'
-  get 'cooking_materials/create'
-  get 'cooking_materials/edit'
-  get 'cooking_materials/update'
-  get 'cooking_materials/destroy'
-  get 'materials/index'
-  get 'materials/create'
-  get 'materials/edit'
-  get 'materials/update'
-  get 'materials/destroy'
-  get 'recipes/index'
-  get 'recipes/show'
-  get 'recipes/index'
-  get 'recipes/new'
-  get 'recipes/edit'
-  get 'recipes/update'
-  get 'recipes/destroy'
-  get 'recipe/index'
-  get 'recipe/show'
+
   devise_for :experts
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'homes#top'
+
+  namespace :experts do
+    resources :recipes, except: :show do
+      #cooking_materialsのルート
+      get '/cooking_materials' => 'cooking_materials#index', as: 'materials'
+      post '/cooking_materials' => 'cooking_materials#create', as: 'material'
+      patch '/cooking_materials/:id' => 'cooking_materials#update'
+      delete'/cooking_materials/:id' => 'cooking_materials#destroy'
+      get '/cooking_materials/:id/edit' => 'cooking_materials#edit', as: 'edit_material'
+      #cooking_proceduresのルート
+      get '/cooking_procedures' => 'cooking_procedures#index', as: 'procedures'
+      post '/cooking_procedures' => 'cooking_procedures#create', as: 'procedure'
+      patch '/cooking_procedures/:id' => 'cooking_procedures#update'
+      delete'/cooking_procedures/:id' => 'cooking_procedures#destroy'
+      get '/cooking_procedures/:id/edit' => 'cooking_procedures#edit', as: 'edit_procedure'
+
+      #resources :cooking_materials, only:[:index, :create, :edit, :update, :destroy]
+      #resources :cooking_procedures, only:[:index, :create, :edit, :update, :destroy]
+    end
+  end
+
+  namespace :users do
+    resources :users, only: [:show, :edit, :update]
+    resources :recipes, only: [:show, :index] do
+      #cooking_materialsのルート
+      get '/cooking_materials' => 'cooking_materials#index', as: 'materials'
+      #cooking_proceduresのルート
+      get '/cooking_procedures' => 'cooking_procedures#index', as: 'procedures'
+      #レビュー機能
+      post '/recipe_favorites' => 'recipe_reviews#create', as: 'review'
+      #ブックマーク機能
+      get '/recipe_favorites' => 'recipe_favorites#index', as: 'favorites'
+      post '/recipe_favorites' => 'recipe_favorites#create', as: 'favorite'
+      delete '/recipe_favorites/:id' => 'recipe_favorites#destroy'
+    end
+  end
 end
