5e25afce2dca12406d52a871e884aad7ccd2da07
yuri0427a <yuri0427ita@gmail.com>
2020-03-22 12:16:09 +0000

    [Deleted]view,[Add]users_show

--- app/controllers/users/recipes_controller.rb
+++ app/controllers/users/recipes_controller.rb
@@ -1,4 +1,4 @@
-class RecipesController < ApplicationController
+class Users::RecipesController < ApplicationController
   def index
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
 
   devise_for :experts
   devise_for :users
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'homes#top'
 
