07ab0a22bf5cba35ef2430fcc8b3fb6c8f78a42e
yuri0427a <yuri0427ita@gmail.com>
2020-03-29 11:59:58 +0000

    [add]モーダルウィンドウメモ

--- app/assets/javascripts/users/user.js
+++ app/assets/javascripts/users/user.js
@@ -12,22 +12,27 @@ $(function () {
     function showModal(event) {
         event.preventDefault();
         
-
+　　　　　//③シェードの生成
         var $shade = $('<div></div>')
         $shade
         .attr('id', 'shade')
-        .on('click', hideModal);
+        .on('click', hideModal);　//3クリックされ時呼び出す設定
         
         var $modalWin = $('#modalwin');
         var $window = $(window);
-        var posX = ($window.width() - $modalWin.outerWidth()) / 2;
-        var posY = ($window.height() - $modalWin.outerHeight()) / 2;
-
+        //②全体の高さ（幅）-モーダルウィンドウの高さ（幅）/2 =　中央ぞろえ
+        var posX = ($window.width() - $modalWin.outerWidth()) / 2;　　//★
+        var posY = ($window.height() - $modalWin.outerHeight()) / 2;　//★
+        //②outwidth(heigth)・・・モーダルウィンドウの幅(高さ)
+     
         $modalWin
-            .before($shade)
-            .css({left: posX, top: posY})
-            .removeClass('out')
-            .addClass('show')
+            .before($shade)　//③divをモーダルウィンドウ前に生成させる→シェードも一緒に表示させる
+            .css({ left: posX, top: posY }) //②★で座標の計算
+           //①モーダルウィンドウを表示する働き（Bのdisplayを操作）
+            .removeClass('out')  //outを消して→none取り消し
+           //①display: block;
+            .addClass('show')　　//showを追加する→blockにする
+       　　 //①イベントバンドラでbutttonウィンドウを閉じられるようにしている ③modal-closeでシェードも閉じられるようにする
             .on('click', 'button, .modal-close', function(){
                 hideModal();
             });
@@ -38,6 +43,6 @@ $(function () {
             .removeClass('show')
             .addClass('hide');
       }
-
+   //編集するボタン（show-modal）を押すと呼び出される（A）
     $('.show-modal').on('click', showModal);
 });
