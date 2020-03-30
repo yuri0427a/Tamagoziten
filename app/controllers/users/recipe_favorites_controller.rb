d67b47b344bc58050a5bdc3b937a411dd6c21731
yuri0427a <yuri0427ita@gmail.com>
2020-03-30 12:37:42 +0000

    [Add]ブックマーク機能

--- app/controllers/users/recipe_favorites_controller.rb
+++ app/controllers/users/recipe_favorites_controller.rb
@@ -1,10 +1,20 @@
 class Users::RecipeFavoritesController < ApplicationController
     def index
+        @user = current_user
+        @bookmarks = RecipeFavorite.where(user_id: @user.id).all
     end
 
     def create
+        recipe = Recipe.find(params[:recipe_id])
+        favorite = current_user.recipe_favorites.new(recipe_id: recipe.id)
+        favorite.save
+        redirect_back(fallback_location: root_path)
     end
 
     def destroy
+        recipe = Recipe.find(params[:recipe_id])
+        favorite = current_user.recipe_favorites.find_by(recipe_id: recipe.id)
+        favorite.destroy
+        redirect_back(fallback_location: root_path)
     end
 end
