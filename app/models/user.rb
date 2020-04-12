b464317040ad0aad42bacd51ec672f153460fd76
yuri0427a <yuri0427ita@gmail.com>
2020-04-12 06:34:00 +0000

    [Add]ニックネームバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         attachment :profile_image
+  attachment :profile_image
   has_many :recipes, dependent: :destroy
   has_many :recipe_favorites, dependent: :destroy
   has_many :recipe_reviews, dependent: :destroy
 
 
- validates :name, presence: true, length: {maximum: 10}
+ validates :name, presence: true, length: {maximum: 10}, format: { with: /\A[a-zA-Z0-9]+\z/, message: 'は半角英数字で入力してください'}
  validates :introduction, length: {maximum: 20}
+
 end
