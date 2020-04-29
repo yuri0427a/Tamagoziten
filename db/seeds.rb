69ab8dd641325e6e4e62fd7fd7d220ec4c6b3110
yuri0427a <yuri0427ita@gmail.com>
2020-04-29 05:12:00 +0000

    [Add]seedデータ

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Category.create!(
+    [
+      {
+        name: 'お肉×たまご',
+      },
+      {
+        name: '野菜×たまご',
+      }
+    ]
+  )
