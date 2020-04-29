a7adbee05c669c088b5f1bef623939db961c6516
yuri0427a <yuri0427ita@gmail.com>
2020-04-29 05:00:22 +0000

    [Add]カテゴリーカラム

--- db/migrate/20200429044905_create_recipe_category_relations.rb
+++ db/migrate/20200429044905_create_recipe_category_relations.rb
@@ -1,7 +1,8 @@
 class CreateRecipeCategoryRelations < ActiveRecord::Migration[5.2]
   def change
     create_table :recipe_category_relations do |t|
-
+      t.integer :recipe_id
+      t.integer :category_id
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_16_083702) do
+ActiveRecord::Schema.define(version: 2020_04_29_044905) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "cooking_materials", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -26,6 +32,13 @@ ActiveRecord::Schema.define(version: 2020_04_16_083702) do
     t.integer "recipe_id", null: false
     t.integer "step_number"
     t.string "procedure_description", null: false
+  end
+
+  create_table "recipe_category_relations", force: :cascade do |t|
+    t.integer "recipe_id"
+    t.integer "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "recipe_favorites", force: :cascade do |t|
