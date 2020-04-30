b2402ed5fba98cb82bccdb665be5d7a68fcae4f1
板谷優里 <itayayuri@MacBook-Pro.local>
2020-04-30 20:52:38 +0900

    [Fixed]記入ミス

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,8 @@
 class Category < ApplicationRecord
-    has_many :reipe_category_relations
+    # 多と多アソシエーション
+    has_many :recipe_category_relations
     has_many :recipes, through: :recipe_category_relations
-
+    
+    # バリデーション
     validates :name, presence: true, uniqueness: true
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,18 +1,23 @@
 class Recipe < ApplicationRecord
-    
+    #アソシエーション（親）
     belongs_to :user
+    # アソシエーション（子）
     has_many :cooking_materials, dependent: :destroy
     has_many :cooking_procedures, dependent: :destroy
     has_many :recipe_favorites, dependent: :destroy
     has_many :recipe_reviews, dependent: :destroy
     has_many :recipe_category_relations
     has_many :categories, through: :recipe_category_relations
+    # レシピ投稿画像 refile定義
     attachment :recipe_image
+    # cocoon定義
     accepts_nested_attributes_for :cooking_materials
     accepts_nested_attributes_for :cooking_procedures
+    # いいね定義
     def favorited_by?(user)
     recipe_favorites.where(user_id: user.id).exists?
     end
+    # バリデーション
     validates :name, presence: true, length: {maximum: 15}
     validates :description, length: {maximum: 30}
     validates :egg_quantity, presence: true
