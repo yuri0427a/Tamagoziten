6de99d997d13d0713d6c7f46e7fe3a01e5f47fee
板谷優里 <itayayuri@MacBook-Pro.local>
2020-05-01 21:03:24 +0900

    [Add]imageカラムtoカテゴリーテーブル

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 2020_04_29_044905) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name", null: false
+    t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
