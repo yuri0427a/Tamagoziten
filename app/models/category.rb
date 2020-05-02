0599273b7695f8a5e62da18e2713a399c17ab3df
板谷優里 <itayayuri@MacBook-Pro.local>
2020-05-02 14:58:19 +0900

    [Modifeid]Uploaderネーム

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,7 +2,7 @@ class Category < ApplicationRecord
     # 多と多アソシエーション
     has_many :recipe_category_relations
     has_many :recipes, through: :recipe_category_relations
-    mount_uploader :image, ImageUploader
+    mount_uploader :image, ImagesUploader
     
     # バリデーション
     validates :name, presence: true, uniqueness: true
