6e8b33f09d52f00cd0adc25dd06375158d0caea6
板谷優里 <itayayuri@MacBook-Pro.local>
2020-05-04 17:32:26 +0900

    [Deleted]gem carrerwave

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,9 +1,7 @@
 class Category < ApplicationRecord
     # 多と多アソシエーション
     has_many :recipe_category_relations
-    has_many :recipes, through: :recipe_category_relations
-    mount_uploader :category_image, ImagesUploader
-    
+    has_many :recipes, through: :recipe_category_relations   
     # バリデーション
     validates :name, presence: true, uniqueness: true
 end
